ef48afa3933c809675f27e6cfe796365d5662d33
Oladokun Oladipupo <ola@5centscdn.com>
2023-05-16 15:49:36 +0100

    maintenance update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '25-tech';
+  date: any;
+  now: any;
+  targetDate: any = new Date(2023, 4, 23);
+  targetTime: any = this.targetDate.getTime();
+  difference!: number;
+  months: Array<string> = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  currentTime: any = `${this.months[this.targetDate.getMonth()]
+    } ${this.targetDate.getDate()}, ${this.targetDate.getFullYear()}`;
+
+  @ViewChild('days', { static: true }) days!: ElementRef;
+  @ViewChild('hours', { static: true }) hours!: ElementRef;
+  @ViewChild('minutes', { static: true }) minutes!: ElementRef;
+  @ViewChild('seconds', { static: true }) seconds!: ElementRef;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.tickTock();
+      this.difference = this.targetTime - this.now;
+      this.difference = this.difference / (1000 * 60 * 60 * 24);
+
+      !isNaN(this.days.nativeElement.innerText)
+        ? (this.days.nativeElement.innerText = Math.floor(this.difference))
+        : (this.days.nativeElement.innerHTML = `<img width="50" src="https://i.gifer.com/VAyR.gif" />`);
+    }, 1000);
+  }
+
+  tickTock() {
+    this.date = new Date();
+    this.now = this.date.getTime();
+    this.days.nativeElement.innerText = Math.floor(this.difference);
+    this.hours.nativeElement.innerText = 23 - this.date.getHours();
+    this.minutes.nativeElement.innerText = 60 - this.date.getMinutes();
+    this.seconds.nativeElement.innerText = 60 - this.date.getSeconds();
+  }
 }
